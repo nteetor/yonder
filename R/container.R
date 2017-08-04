1fb2d8533b0d4ad0d34415fba9c34cdfae42d118
nteetor <nathanteetor@gmail.com>
2017-08-04 09:26:51 -0400

    typo fix

--- R/container.R
+++ R/container.R
@@ -1,4 +1,4 @@
-#' Grid ystem and esponsive layout
+#' Grid system and responsive layout
 #'
 #' Flexbox, grid layout, containers, rows, and columns (oh my). Arguments `width` and
 #' `order` can be applied depending on the width of the page, see the details
