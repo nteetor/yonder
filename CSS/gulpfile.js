46c549e2c16416b481f4a6ffc83857e8aaa0534b
nteetor <nathanteetor@gmail.com>
2017-08-15 12:56:57 -0400

    fixed css gulp, added back missing font awesome files

--- CSS/gulpfile.js
+++ CSS/gulpfile.js
@@ -3,14 +3,23 @@ var gulp = require("gulp");
 
 /* PACKAGES */
 var clean = require("gulp-clean-css"),
+    sass = require("gulp-sass"),
     concat = require("gulp-concat");
 
 /* DIRECTORIES */
-var cssFiles = "styles/*.css",
+var scssFiles = "scss/*.scss",
+    cssLocal = "./css",
+    cssFiles = cssLocal + "/*.css",
     cssDest = "../inst/www/css";
 
 /* TASKS */
-gulp.task("clean-css", function() {
+gulp.task("sass", function() {
+  return gulp.src(scssFiles)
+    .pipe(sass().on("error", sass.logError))
+    .pipe(gulp.dest(cssLocal));
+});
+
+gulp.task("clean", function() {
   return gulp.src(cssFiles)
     .pipe(concat("dull.min.css"))
     .pipe(clean({
@@ -19,4 +28,4 @@ gulp.task("clean-css", function() {
     .pipe(gulp.dest(cssDest));
 });
 
-gulp.task("default", ["clean-css"]);
+gulp.task("default", ["sass", "clean"]);
--- R/includes.R
+++ R/includes.R
@@ -11,17 +11,19 @@ bootstrap <- function() {
       ),
       tags$script(src = "dull/bootstrap/bootstrap.min.js"),
       tags$script(src = "dull/js/dull.min.js"),
-      tags$link(rel = "stylesheet", href = "dull/css/dull.css")
+      tags$link(rel = "stylesheet", href = "dull/css/dull.min.css")
     )
   )
 }
 
 `font-awesome` <- function() {
+  shiny::addResourcePath("dull", system.file("www", package = "dull"))
+
   htmltools::singleton(
     tags$head(
       tags$link(
         rel = "stylesheet",
-        href = "dull/font-awesome/font-awesome.min.css"
+        href = "dull/font-awesome/css/font-awesome.min.css"
       )
     )
   )
