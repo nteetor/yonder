c07792c253656d215d901ae49f4fda8d4e05ebc8
nteetor <nathanteetor@gmail.com>
2017-08-17 12:13:34 -0400

    hoping to fix wacky gulp issues

--- CSS/gulpfile.js
+++ CSS/gulpfile.js
@@ -2,7 +2,8 @@
 var gulp = require("gulp");
 
 /* PACKAGES */
-var clean = require("gulp-clean-css"),
+var css = require("gulp-clean-css"),
+    clean = require("gulp-clean"),
     sass = require("gulp-sass"),
     concat = require("gulp-concat");
 
@@ -13,19 +14,24 @@ var scssFiles = "scss/*.scss",
     cssDest = "../inst/www/css";
 
 /* TASKS */
+gulp.task("clean", function() {
+  return gulp.src(cssFiles, {read: false})
+    .pipe(clean());
+});
+  
 gulp.task("sass", function() {
   return gulp.src(scssFiles)
     .pipe(sass().on("error", sass.logError))
     .pipe(gulp.dest(cssLocal));
 });
 
-gulp.task("clean", function() {
+gulp.task("concat", function() {
   return gulp.src(cssFiles)
     .pipe(concat("dull.min.css"))
-    .pipe(clean({
+    .pipe(css({
       compatibility: "ie9"
     }))
     .pipe(gulp.dest(cssDest));
 });
 
-gulp.task("default", ["sass", "clean"]);
+ gulp.task("default", ["clean", "sass", "concat"]);
