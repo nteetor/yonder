9c855f6444f2445cdf3fd8d984dc6ffd42151e96
nteetor <nathanteetor@gmail.com>
2017-11-21 17:12:22 -0500

    adding/building range input, css needs tuning

---
+++ R/range.R
@@ -0,0 +1,41 @@
+#' A range or slider input
+#'
+#' Create a range input.
+#'
+#' @param id A character string specifying the id of the range input.
+#'
+#' @export
+#' @examples
+#' if (interactive()) {
+#'   shinyApp(
+#'     ui = container(
+#'       row(
+#'         col(
+#'           rangeInput("range1"),
+#'           rangeInput("range2", context = "primary"),
+#'           rangeInput("range3", context = "secondary"),
+#'           rangeInput("range4", context = "success")
+#'         ),
+#'         col(
+#'         )
+#'       )
+#'     ),
+#'     server = function(input, output) {
+#'
+#'     }
+#'   )
+#' }
+#'
+rangeInput <- function(id, context = NULL) {
+  tags$div(
+    class = "dull-input dull-range-input form-group",
+    tags$input(
+      type = "range",
+      id = id,
+      class = collate(
+        "range-default",
+        if (!is.null(context)) paste0("range-", context)
+      )
+    )
+  )
+}
