f4ab53c50540422f5fe1307b51ae7d85cb76fc5f
nteetor <nathanteetor@gmail.com>
2017-11-27 14:40:55 -0500

    initial range input css and R function complete

--- R/range.R
+++ R/range.R
@@ -4,6 +4,36 @@
 #'
 #' @param id A character string specifying the id of the range input.
 #'
+#' @param min The minimum value of the range.
+#'
+#' @param max The maxmium value of the range.
+#'
+#' @param step Controls possible values, the values of the range input are
+#'   multiples of `step`. Defaults to `1`. If `length` is specified the `step`
+#'   argument is ignored.
+#'
+#' @param length An alternative to `step`, a number specifying how many values
+#'   the range input has. Defaults to `NULL`, in which case `step` is used.
+#'
+#' @param default The default value of the range input. Defaults to `NULL` in
+#'   which case the default value is midway between the minium and maximum
+#'   values.
+#'
+#' @param labels A character vector of two labels or a list of two tag elements
+#'   to use as labels for the minimum and maximum of the range. Defaults to
+#'   `NULL` in which case `min` and `max` are used as the labels.
+#'
+#' @param context One of `"primary"`, `"secondary"`, `"success"`, `"info"`,
+#'   `"warning"`, or `"danger"` specifying the visual context of the range
+#'   input.
+#'
+#'   **Note**: On Chrome the entire range input is colored according to the
+#'   context. On Firefox and Internet Explorer the lower half of the range input
+#'   is colored according to the context.
+#'
+#' @param thumb One of `"square"` or `"rounded"` specifying the appearance of
+#'   the thumb. Defaults to `NULL` in which case the thumb is circular.
+#'
 #' @export
 #' @examples
 #' if (interactive()) {
@@ -11,11 +41,47 @@
 #'     ui = container(
 #'       row(
 #'         col(
-#'           rangeInput("range1"),
-#'           rangeInput("range2", context = "primary"),
-#'           rangeInput("range3", context = "secondary"),
-#'           rangeInput("range4", context = "success"),
-#'           rangeInput("range5", context = "info", round = TRUE)
+#'           class = "col col-4 mx-auto",
+#'           rangeInput(
+#'             id = "baz",
+#'             size = "large"
+#'           ),
+#'           rangeInput(
+#'             id = "foo",
+#'             default = 60
+#'           ),
+#'           rangeInput(
+#'             id = "bar",
+#'             size = "small"
+#'           )
+#'         )
+#'       )
+#'     ),
+#'     server = function(input, output) {
+#'
+#'     }
+#'   )
+#' }
+#'
+#' if (interactive()) {
+#'   shinyApp(
+#'     ui = container(
+#'       row(
+#'         col(
+#'           rangeInput(
+#'             id = "range1",
+#'             context = "primary",
+#'             labels = c("Minimum", "Maximum")
+#'           ),
+#'           rangeInput(
+#'             id = "range2",
+#'             context = "warning"
+#'           ),
+#'           rangeInput(
+#'             id = "range3",
+#'             length = 4,
+#'             context = "info"
+#'           )
 #'         ),
 #'         col(
 #'         )
@@ -27,15 +93,43 @@
 #'   )
 #' }
 #'
-rangeInput <- function(id, context = NULL, round = FALSE) {
+rangeInput <- function(id, min = 0, max = 100, step = 1, length = NULL,
+                       default = NULL, labels = NULL,
+                       context = NULL, size = NULL, track = NULL,
+                       thumb = NULL) {
+  if (!is.null(length)) {
+    step <- diff(seq(min, max, length.out = length))[1]
+  }
+
   tags$div(
     class = "dull-input dull-range-input form-group",
+    tags$div(
+      class = "labels",
+      tags$label(
+        `for` = id,
+        if (!is.null(labels)) labels[[1]] else min
+      ),
+      tags$label(
+        `for` = id,
+        if (!is.null(labels)) labels[[2]] else max
+      )
+    ),
     tags$input(
       type = "range",
       id = id,
+      min = min,
+      max = max,
+      step = step,
+      value = default,
       class = collate(
+        "range",
         if (!is.null(context)) paste0("range-", context),
-        if (round) "thumb-circle"
+        if (!is.null(size)) {
+          paste0("range-", switch(size, small = "sm", large = "lg"))
+        },
+        if (!is.null(track)) paste0("track-", track),
+        # if (vertical) "range-vertical",
+        if (!is.null(thumb)) paste0("thumb-", thumb)
       )
     )
   )
