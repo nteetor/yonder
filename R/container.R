8fdb9feced5936890df33b7845de8036d48139d7
nteetor <nathanteetor@gmail.com>
2017-12-01 17:56:49 -0500

    revisted and fixed the long wonked col() function

--- R/container.R
+++ R/container.R
@@ -1,33 +1,93 @@
 #' Grid system and responsive layout
 #'
-#' Flexbox, grid layout, containers, rows, and columns (oh my). Arguments `width` and
-#' `order` can be applied depending on the width of the page, see the details
-#' section for more information.
+#' Build apps using the grid layout: containers, rows, and columns (oh my).
 #'
 #' @param ... Any number of child elements or named arguments passed as HTML
 #'   attributes to the parent element. `row`s need to be placed inside a
 #'   `container`. A `row` typically contains only `col`s. A `col` may contain
-#'   other `col`s or any number of other elements.
+#'   other `col`s or other elements.
+#'
+#' @param tag A tag element, for `col` this is typically `tags$div()`.
+#'
+#' @param default A number 1 through 12 or `"auto"` specifying the default
+#'   width of the column. Columns with width `"auto"` equally divide space or
+#'   fill remaining space when used with other columns.
+#'
+#' @param sm Like `default`, but the width is applied once the viewport is 576
+#'   pixels wide, think phone in landscape mode.
+#'
+#' @param md Like `default`, but the width is applied once the viewport is 768
+#'   pixels wide, think tablets.
+#'
+#' @param lg Like `default`, but the width is applied once the viewport is 992
+#'   pixels wide, think desktop.
+#'
+#' @param xl Like `default`, but the width is applied once the viewport is 1200
+#'   pixels wide, think large desktop.
 #'
 #' @export
 #' @examples
+#' if (interactive()) {
+#'   shinyApp(
+#'     ui = container(
+#'       row(
+#'         col("1 of 2") %>%
+#'           border(),
+#'         col("2 of 2") %>%
+#'           border()
+#'       ),
+#'       row(
+#'         lapply(
+#'           1:3,
+#'           . %>%
+#'             paste("of 3") %>%
+#'             col() %>%
+#'             border()
+#'         )
+#'       )
+#'     ),
+#'     server = function(input, output) {
 #'
-#' stub
+#'     }
+#'   )
+#' }
 #'
-col <- function(..., default = NULL, small = NULL, medium = NULL,
-                large = NULL, extralarge = NULL) {
+#' if (interactive()) {
+#'   shinyApp(
+#'     ui = container(
+#'       row(
+#'         col("1 or 3") %>%
+#'           border(),
+#'         col("2 of 3", default = 6) %>%
+#'           border(),
+#'         col("3 of 3") %>%
+#'           border()
+#'       )
+#'     ),
+#'     server = function(input, output) {
+#'
+#'     }
+#'   )
+#' }
+#'
+col <- function(..., default = NULL, sm = NULL, md = NULL, lg = NULL,
+                xl = NULL) {
+  args <- dropNulls(list(default = default, sm = sm, md = md, lg = lg, xl = xl))
 
-  tags$div(
-    class = collate(
-      default %||% "col",
-      small,
-      medium,
-      large,
-      extralarge
-    ),
-    ...,
-    bootstrap()
+  if (length(args) == 0) {
+    return(tagEnsureClass(tags$div(...), "col"))
+  }
+
+  classes <- responsives(
+    prefix = "col",
+    values = args,
+    possible = c(as.character(1:12), "auto")
   )
+
+  classes <- sub("-((sm|md|lg|xl)-)?auto", "", classes)
+  classes <- unique(classes)
+
+  tagAddClass(tags$div(...), collate(classes))
 }
 
 #' @rdname col
@@ -46,6 +106,7 @@ container <- function(..., fluid = TRUE) {
   tags$div(
     class = if (fluid) "container-fluid" else "container",
     ...,
-    bootstrap()
+    bootstrap(),
+    `font-awesome`()
   )
 }
