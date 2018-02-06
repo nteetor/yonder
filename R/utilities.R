c18bf0e37a4df399e75458475c2fbfe64f3a5759
nteetor <nathanteetor@gmail.com>
2018-02-06 12:41:46 -0500

    removed context argument from alerts, re #32

--- R/utilities.R
+++ R/utilities.R
@@ -1,22 +1,15 @@
 .colors <- c(
   "red",
-  # "pink",
   "purple",
-  # "deep-purple",
   "indigo",
   "blue",
-  # "light-blue",
   "cyan",
   "teal",
   "green",
-  # "light-green",
-  # "lime",
   "yellow",
   "amber",
   "orange",
-  # "deep-orange",
   "brown",
-  # "blue-grey",
   "grey"
 )
 
--- module/lib/js/dull.js
+++ module/lib/js/dull.js
@@ -45,7 +45,7 @@ Shiny.addCustomMessageHandler("dull:alert", function(msg) {
   var ids = msg.id.map(v => "#" + v).join(",");
 
   $("<div>")
-    .addClass("alert alert-dismissible alert-" + msg.context + " fade show")
+    .addClass("alert alert-dismissible fade show")
     .html(msg.content)
     .append(
       $("<button>", {
