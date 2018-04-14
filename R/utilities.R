63cb5f9d273d51d2016a2ab26a48e39c656d4119
nteetor <nathanteetor@gmail.com>
2018-04-13 21:15:18 -0400

    added shadow utility, re #60

--- R/utilities.R
+++ R/utilities.R
@@ -371,6 +371,62 @@ rounded <- function(tag, sides = "all") {
   tag
 }
 
+#' Add shadows to tag elements
+#'
+#' The `shadow` utility applies a shadow to a tag element. Elements with a
+#' shadow may appear to pop off the page. The material design set of components,
+#' used on Android and for Google applications, commonly uses shadowing.
+#' Although `"none"` is an allowed `size`, most elements do not have a shadow by
+#' default.
+#'
+#' @param tag A tag element.
+#'
+#' @param size One of `"none"`, `"small"`, `"regular"`, or `"large"` specifying
+#'   the amount of shadow added, defaults to `"regular"`.
+#'
+#' @export
+#' @examples
+#'
+#' if (interactive()) {
+#'   shinyApp(
+#'     ui = tagList(
+#'       navbar(brand = "Navbar") %>%
+#'         background("cyan", +1) %>%
+#'         shadow("small"),
+#'       container(
+#'         "Cras mattis consectetur purus sit amet fermentum. Donec sed ",
+#'         "odio dui. Lorem ipsum dolor sit amet, consectetur adipiscing ",
+#'         "elit. Aenean eu leo quam. Pellentesque ornare sem lacinia quam ",
+#'         "venenatis vestibulum."
+#'       ) %>%
+#'         padding(3)
+#'     ),
+#'     server = function(input, output) {
+#'
+#'     }
+#'   )
+#' }
+#'
+shadow <- function(tag, size = "regular") {
+  if (!re(size, "none|small|regular|large", len0 = FALSE)) {
+    stop(
+      "invalid `shadow()` argument, `size` must be one of ",
+      '"none", "small", "regular", or "large"',
+      call. = FALSE
+    )
+  }
+
+  size <- switch(
+    size,
+    none = "none",
+    small = "sm",
+    regular = NULL,
+    large = "lg"
+  )
+
+  tagEnsureClass(tag, paste0(c("shadow", size), collapse = "-"))
+}
+
 #' Float an element
 #'
 #' The `float` utility function applies Bootstrap float classes to a tag
