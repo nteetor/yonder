7445bd6d30ff64676a07b096037701e26abf5932
nteetor <nathanteetor@gmail.com>
2018-10-20 17:59:38 -0400

    chore(imports): import from htmltools, not shiny

--- R/yonder.R
+++ R/yonder.R
@@ -2,9 +2,10 @@
 #'
 #' More freedom to the programmer, more to come.
 #'
-#' @importFrom htmltools htmlDependency attachDependencies suppressDependencies
-#' @importFrom shiny observe getDefaultReactiveDomain tagAppendAttributes tags
-#'   HTML installExprFunction createRenderFunction
+#' @importFrom htmltools HTML htmlDependency attachDependencies
+#'   suppressDependencies tags tagAppendAttributes tagAppendChildren
+#' @importFrom shiny observe getDefaultReactiveDomain installExprFunction
+#'   createRenderFunction
 #' @importFrom rlang dots_list
 #'
 #' @name yonder
