937cd55d5975187423235798c3bf7d4f62a85a98
nteetor <nathanteetor@gmail.com>
2018-12-07 17:08:30 -0500

    fix(radiobarInput): missing bindind methods
    * radiobar input binding now includes update, enable, and disable methods

--- module/src/js/input-binding-radiobar.js
+++ module/src/js/input-binding-radiobar.js
@@ -13,6 +13,45 @@ $.extend(radiobarInputBinding, {
   ],
   getState: function(el, data) {
     return { value: this.getValue(el) };
+  },
+  _update: function(el, data) {
+    let children = el.querySelectorAll(".btn");
+
+    children.forEach((child, i) => {
+      child.querySelector("span").innerHTML = data.choices[i];
+      child.querySelector("input").setAttribute("data-value", data.values[i]);
+    });
+  },
+  _enable: function(el, data) {
+    let children = el.querySelectorAll(".btn");
+
+    children.forEach(child => {
+      let input = child.querySelector("input");
+      let value = input.getAttribute("data-value");
+      let index = data.values ? data.values.indexOf(value) : 0;
+
+      if ((index > -1) === !data.invert) {
+        child.classList.remove("disabled");
+        input.removeAttribute("disabled");
+      }
+    });
+  },
+  _disable: function(el, data) {
+    let children = el.querySelectorAll(".btn");
+
+    children.forEach(child => {
+      let input = child.querySelector("input");
+      let value = input.getAttribute("data-value");
+      let index = data.values ? data.values.indexOf(value) : 0;
+
+      if ((index > -1) === !data.invert) {
+        child.classList.add("disabled");
+        input.setAttribute("disabled", "");
+      } else if (data.reset) {
+        child.classList.remove("disabled");
+        input.removeAttribute("disabled");
+      }
+    });
   }
 });
 
