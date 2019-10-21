42b5fe93feba2a963de6711be0d5886cd5c6a430
nteetor <nathanteetor@gmail.com>
2019-10-21 10:21:00 -0400

    fix(file): skeleton, events in progress

--- js/src/index.js
+++ js/src/index.js
@@ -3,7 +3,7 @@ import ButtonInput from "./inputs/button.js";
 import CheckbarInput from "./inputs/checkbar.js";
 import CheckboxInput from "./inputs/checkbox.js";
 // import "./input-binding-chip.js";
-// import "./input-binding-file.js";
+import FileInput from "./inputs/file.js";
 // import "./input-binding-form.js";
 // import "./input-binding-link.js";
 // import "./input-binding-list-group.js";
@@ -28,6 +28,7 @@ export default {
   ButtonInput,
   CheckbarInput,
   CheckboxInput,
+  FileInput,
   MenuInput,
   NavInput,
   RadioInput
--- js/src/inputs/file.js
+++ js/src/inputs/file.js
@@ -25,6 +25,13 @@ const Selector = {
   PROGRESS: `.${ ClassName.PROGRESS }`
 }
 
+const Event = {
+  DRAGOVER: `dragover.${ TYPE }`,
+  DRAGCENTER: `dragcenter.${ TYPE }`,
+  DROP: `drop.${ TYPE }`,
+  CHANGE: `change.${ TYPE }`
+}
+
 class FileInput extends Input {
 
   // methods ----
@@ -33,4 +40,76 @@ class FileInput extends Input {
     super(element, TYPE)
   }
 
-};
+  value() {
+    return null
+  }
+
+  upload() {
+    let files = this._element.files
+
+    return files
+  }
+
+  // static ----
+
+  static initialize(element) {
+    super.initialize(element, TYPE, FileInput)
+  }
+
+  static find(scope) {
+    return super.find(scope, Selector.INPUT)
+  }
+
+  static getValue(element) {
+    return super.getValue(element, TYPE)
+  }
+
+  static subscribe(element, callback) {
+    super.subscribe(element, callback, TYPE)
+  }
+
+  static unsubscribe(element) {
+    super.unsubscribe(element, TYPE)
+  }
+
+  static ShinyInterface() {
+    return { ...Input, ...FileInput }
+  }
+
+}
+
+// events ----
+
+$(document).on(Event.DRAGOVER, Selector.INPUT, (event) => {
+  event.stopPropagation()
+  event.preventDefault()
+})
+
+$(document).on(Event.DRAGOVER, Selector.INPUT, (event) => {
+  event.stopPropagation()
+  event.preventDefault()
+})
+
+$(document).on(Event.DROP, Selector.INPUT, (event) => {
+  event.stopPropagation()
+  event.preventDefault()
+
+  let el = findClosest(event.target, Selector.INPUT)
+  let fileInput = Store.getData(el, TYPE)
+
+  if (!fileInput) {
+    return
+  }
+
+  // upload
+})
+
+$(document).on(Event.CHANGE, Selector.INPUT, (event) => {
+  console.log("change")
+})
+
+export default FileInput
+
+if (Shiny) {
+  Shiny.inputBindings.register(FileInput.ShinyInterface(), TYPE)
+}
