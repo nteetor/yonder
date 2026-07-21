73b21970dd1659d85a75d99db87eb12017f42218
nteetor <nate@haufin.ch>
2026-07-21 21:17:40 +0900

    update(alert): type arg, better dismiss

--- R/alert.R
+++ R/alert.R
@@ -5,7 +5,12 @@
 #' @param ... Components to include. Named arguments are passed as HTML
 #'   attributes to the parent element.
 #'
-#' @param class Additional CSS classes for the alert element.
+#' @param type Optional semantic type.
+#'
+#' @param dismiss The method for removing the alert. One of `"animate"`,
+#' `"instant"`, or `"none"`.
+#'
+#' @param dismiss_button The dismiss button element. A [htmltools::tag] object.
 #'
 #' @param container An [htmltools::tag] function.
 #'
@@ -18,7 +23,8 @@
 #' @examplesIf rlang::is_interactive()
 #'
 #' alert(
-#'   class = "alert-info",
+#'   type = "info",
+#'   dismiss = "none",
 #'   alert_heading("Heads up!"),
 #'   "Make sure you know the thing is happening",
 #'   htmltools::hr(),
@@ -26,59 +32,80 @@
 #' )
 #'
 #' alert(
-#'   class = "alert-warning",
-#'   "Double check those figures.",
-#'   alert_button()
+#'   type = "warning",
+#'   "Double check those figures."
 #' )
 #'
-alert <- function(
-  ...,
-  class = NULL
-) {
-  component <-
-    tags$div(
-      class = "alert",
-      role = "alert",
-      class = class,
-      ...
-    )
+alert <-
+  function(
+    ...,
+    type = NULL,
+    dismiss = c("animate", "instant", "none"),
+    dismiss_button = alert_button()
+  ) {
+    if (!is.null(type)) {
+      type <-
+        arg_match(
+          type,
+          c(
+            "primary",
+            "secondary",
+            "success",
+            "info",
+            "warning",
+            "danger",
+            "light",
+            "dark"
+          )
+        )
+    }
+    dismiss <- arg_match(dismiss)
 
-  query <-
-    htmltools::tagQuery(component)
-
-  if (query$find(".btn-close")$length() > 0) {
     component <-
-      query$addClass("alert-dismissible fade show")$allTags()
+      tags$div(
+        class = "alert",
+        class = sprintf("alert-%s", type),
+        class = switch(
+          dismiss,
+          animate = "alert-dismissible fade show",
+          instant = "alert-dismissible",
+          none = NULL
+        ),
+        role = "alert",
+        ...,
+        if (dismiss != "none") dismiss_button
+      )
+
+    component <-
+      dependency_append(component)
+
+    component <-
+      s3_class_add(component, "bsides_alert")
+
+    component
   }
 
-  component <-
-    dependency_append(component)
-
-  component <-
-    s3_class_add(component, "bsides_alert")
-
-  component
-}
+#' @rdname alert
+#' @export
+alert_heading <-
+  function(
+    ...,
+    container = htmltools::h5
+  ) {
+    container(
+      class = "alert-heading",
+      ...
+    )
+  }
 
 #' @rdname alert
 #' @export
-alert_heading <- function(
-  ...,
-  container = htmltools::h6
-) {
-  container(
-    class = "alert-heading",
-    ...
-  )
-}
-
-#' @rdname alert
-#' @export
-alert_button <- function() {
-  tags$button(
-    type = "button",
-    class = "btn-close",
-    `data-bs-dismiss` = "alert",
-    `aria-label` = "Close"
-  )
-}
+alert_button <-
+  function() {
+    tags$button(
+      type = "button",
+      class = "btn-close",
+      `data-bs-dismiss` = "alert",
+      `aria-label` = "Close"
+    )
+  }
